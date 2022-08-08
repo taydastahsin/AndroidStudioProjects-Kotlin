5d9de198d4b1d229cc26ab91c2e21660abe5452c
Tahsin Taydaş <56533800+taydastahsin@users.noreply.github.com>
2022-08-08 21:38:11 +0300

    08.08.2022

---
+++ Degiskenler/app/src/main/java/com/taydatahsin/myapplication2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.taydatahsin.myapplication2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Değişkenler
+
+
+    }
+}
