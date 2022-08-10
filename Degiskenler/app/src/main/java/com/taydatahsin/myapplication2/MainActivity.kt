2522ee4d999c67b87f834c444e9223a698cc0a96
Tahsin Taydaş <56533800+taydastahsin@users.noreply.github.com>
2022-08-10 09:03:42 +0300

    10.08.2022

--- Degiskenler/app/src/main/java/com/taydatahsin/myapplication2/MainActivity.kt
+++ Degiskenler/app/src/main/java/com/taydatahsin/myapplication2/MainActivity.kt
@@ -10,6 +10,52 @@ class MainActivity : AppCompatActivity() {
 
         //Değişkenler
 
+        var x=5 // verilen değeri aktif olarak değiştirilebilir
+        val y=5 // verilen değeri değiştirelemez
+
+        //********Integer**********
+         val integerexample :Int//Tanımlama
+
+         integerexample=5 //Değerin verilmesi
+
+        //********Long*********
+        val longExample :Long //tanım(integer 'a sığmayan değerleri tutar)
+
+        longExample =555555555555555 // değer
+
+        //***********Double************
+
+        val doubleExample :Double//ONdalıklı değer tanım
+
+        doubleExample =3.5 //ondalıklı değer
+
+        //***********String***********
+
+        val stringExample :String //Tanımlama
+
+        stringExample="Merhaba Dünya" //String değeri (değeri çift tırnak arasına alıyoruz)
+
+        //*********Char****************
+
+        val charExample :Char //Tanımlama
+
+        charExample='a' //Char değeri (değeri tek tırnak arasına alıyoruz)
+
+        //**************Boolean**************
+
+        var booleanExample =true//Boolean değişkeni doğru ve yanlış argümanlarını kullanır
+        booleanExample=false//Argümanlar 1 ve 0 gibi düşünebiliriz
+
+        // <  -> küçüktür
+        // >  -> büyüktür
+        // =  -> eşittir
+        // != -> eşit değildir
+        // <= -> küçüktür ve eşittir
+        // >= -> büyüktür ve eşittir
+        // && -> ve
+        // || -> veya
+
+
 
     }
 }
