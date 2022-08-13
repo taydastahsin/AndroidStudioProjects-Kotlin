8039d555d1231eee6bd5d2606bc4324b3164d2e3
Tahsin Taydaş <56533800+taydastahsin@users.noreply.github.com>
2022-08-13 09:25:20 +0300

    13.08.2022

--- MyApplication3/app/src/main/java/com/taydatahsin/myapplication3/MainActivity.kt
+++ MyApplication3/app/src/main/java/com/taydatahsin/myapplication3/MainActivity.kt
@@ -2,10 +2,31 @@ package com.taydatahsin.myapplication3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun showHero(view: View){//button click işlemi
+        var isim =heroName.text.toString()//ekrandaki textleri değişkenlere tanımladık
+        var yas =heroYas.text.toString().toIntOrNull()
+        var meslek =heroMeslek.text.toString()
+
+        if (yas !=null){//int değerin null olup olmadığını kontrol ediyor
+            val heroWrite=heroMan(isim,yas,meslek)//heroMan sınıfından değişken tanımladık
+            textView.text="isim : ${heroWrite.isim} yaş : ${heroWrite.yas} meslek : ${heroWrite.meslek} "//değişkenleri textview yazdırıyoruz
+        }
+        else{
+            textView.text="null değeri döndü"//eğer null değer dönerse yazacak
+        }
+
+
+
+
+
     }
 }
---
+++ MyApplication3/app/src/main/java/com/taydatahsin/myapplication3/heroMan.kt
@@ -0,0 +1,6 @@
+package com.taydatahsin.myapplication3
+
+class heroMan(var isim:String,var yas:Int,var meslek:String) {
+
+
+}
