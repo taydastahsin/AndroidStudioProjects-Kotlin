047ff9d89a6bcb5a215e5f68219cad79175a39de
Tahsin Taydaş <56533800+taydastahsin@users.noreply.github.com>
2022-08-13 08:38:42 +0300

    13.08.2022

--- MyApplication2/app/src/main/java/com/taydatahsin/myapplication2/MainActivity.kt
+++ MyApplication2/app/src/main/java/com/taydatahsin/myapplication2/MainActivity.kt
@@ -16,6 +16,10 @@ class MainActivity : AppCompatActivity() {
 
         var changeFun=carpmaFun(3,4)//fonksiyonu değişkene tanımladık
         println(changeFun)//fonksiyonun tanımlı olduğu değişkeni ekrana yazdırdık
+
+        agaclar()
+
+        nulGuvenligi()
     }
 
     // Fonksiyonlar
@@ -41,6 +45,63 @@ class MainActivity : AppCompatActivity() {
         println(toplamaFun(25,30)) //toplam fonksiyonu Logcate yazdırıyoruz
         var a=carpmaFun(8,6)//çarpma fonksiyonu bir değere atanmış
        textView.text="Sonuç : ${a}"//çarpma fonksiyonunu butonla textview e yazdırıyoruz
+    }
+/*
+    fun agaclar(){//agaclar fonksiyonunu tanımlıyoruz
+        var agac=Tree() // "Tree" sınıfını "agac" değişkenine tanımlıyoruz
+        agac.isim="Çam Ağacı"//Sınıfın içindeki değişkenlerin değerlerini giriyoruz
+        agac.yas=15
+        agac.tur="ince yapraklı"
+
+        println(agaclar())
+
+    }
+*/
+    /*
+    fun agaclar(){//fonksiyon tanımladık
+        var agac =Tree("Ceviz Ağacı",25,"Odunsu")//Sınıfın değerlerini yazdırdık
+    textView.text="isim : ${agac.isim}"//"Tree" sınıfının değişkenlerinin değerlerini "agac" değişkeni ile yazdırdık
+    }
+*/
+
+    fun agaclar(){//fonksiyon tanımladık
+        var agac =Tree("Ceviz Ağacı",25,"Odunsu")//Sınıfın değerlerini yazdırdık(Kotline özel)
+        textView.text="isim : ${agac.isim}"//"Tree" sınıfının değişkenlerinin değerlerini "agac" değişkeni ile yazdırdık
+    //Sınıf kısmındaki primary constructor yöntemi ile tek satırda kolaylıkla oluşturabiliriz.
+
+        agac.testClass()//Sınıfın içindeki fonksiyonu çağırdık
+    }
+
+    fun nulGuvenligi(){
+        var benimYas :Int? = null //int değer gelmeyebilir diye belirtiyoruz (Kullanıcının hatalı girişi için)
+        println(benimYas)
+
+       // benimYas =2
+        //1. yöntem if-else konrol ederek yazdırma
+        if (benimYas !=null){
+            println(benimYas)
+        }
+        else{
+            println("null değer döndü")
+        }
+
+        //2 .yöntem null olup(?) olmadığını(!!) belirtiyoruz
+
+        println(benimYas?.minus(5))//null olabilir
+        println(benimYas!!.minus(3))//null olmadığından eminim
+
+        //3.yöntem elvis operatörü
+
+        val sonuc=benimYas?.minus(5) ?:"null değer döndü"
+        println(sonuc)
+
+        //4.yöntem let yöntemi
+        // "benimyas" değişkenini "it" ye atayıp değerini kullanıyoruz
+        benimYas.let {
+            println(it+5)
+        }
+
+
     }
 
 }
---
+++ MyApplication2/app/src/main/java/com/taydatahsin/myapplication2/Tree.kt
@@ -0,0 +1,30 @@
+package com.taydatahsin.myapplication2
+
+class Tree(var isim:String,var yas :Int,var tur:String) {//Primary Constructor (Sınıfın içinde direk gösterim)
+    //Kotline özel bir durum (Yukardaki)
+
+    fun testClass(){
+        println("sınıf çağrıldı.")
+    }
+
+
+
+    /*
+    //Sınıfın içindeki değişkenler
+    var isim=" "
+    var yas= 0
+    var tur=" "
+/////////////////////
+    constructor(isimgirdi:String,yasgirdi:Int,turgirdi:String){//Değişkenleri Main kısmında tanımlamaya gerek kalmadan yazıyoruz
+        //Sınıf değişkenlerini "constructor" içindeki değişl-kenlere tanımladık
+        //tanımlanan değişkenler main kısmında tek satırla tanımlanacaktır
+        //"this" yaptığımı fonksiyonun üstündeki işlemleri yapmamızı sağlar
+        this.isim=isimgirdi
+        this.yas=yasgirdi
+        this.tur=turgirdi
+
+    }
+
+     */
+
+}
