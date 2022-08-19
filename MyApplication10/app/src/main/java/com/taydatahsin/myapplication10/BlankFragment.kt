6c8f54d6e73d1380dcf269b642f9746424b7b5d7
Tahsin Taydaş <56533800+taydastahsin@users.noreply.github.com>
2022-08-19 11:47:12 +0300

    19.08.2022

---
+++ MyApplication10/app/src/main/java/com/taydatahsin/myapplication10/BlankFragment.kt
@@ -0,0 +1,26 @@
+package com.taydatahsin.myapplication10
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class BlankFragment : Fragment() {//Fragment yapısını kullandım
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+
+}
--- MyApplication10/app/src/main/java/com/taydatahsin/myapplication10/MainActivity.kt
+++ MyApplication10/app/src/main/java/com/taydatahsin/myapplication10/MainActivity.kt
@@ -2,10 +2,31 @@ package com.taydatahsin.myapplication10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun forwadFragment(view:View){//ileri buttonu fonksiyonu oluşturdum
+
+        val fragmentManager =supportFragmentManager//fragment destek yöneticisini değişkene atadım
+        val fragmentTransaction=fragmentManager.beginTransaction()//destek yöneticsinden "fragmentManager" çağırdım ve başlatmasını söyledim
+
+        val firsFragment=BlankFragment()//fragment yapısını birinci değişkenini tanımladım
+        fragmentTransaction.replace(R.id.frameLayout2,firsFragment).commit()//fragment yöneticisinden ana ekrandaki "frameLayout2" olarak oluşturduğum
+        //kısma ilk fragment yapısını gösterdim
+
+    }
+    fun backFragment(view: View){//geri buttonu fonksiyonu oluşturdum
+        val fragmentManager =supportFragmentManager//fragment destek yöneticisini değişkene atadım
+        val fragmentTransaction=fragmentManager.beginTransaction()//destek yöneticsinden "fragmentManager" çağırdım ve başlatmasını söyledim
+
+        val firsFragment=BlankFragment2()//fragment yapısını ikinci değişkenini tanımladım
+        fragmentTransaction.replace(R.id.frameLayout2,firsFragment).commit()//fragment yöneticisinden ana ekrandaki "frameLayout2" olarak oluşturduğum
+        //kısma ikinci fragment yapısını gösterdim
+
+
     }
 }
