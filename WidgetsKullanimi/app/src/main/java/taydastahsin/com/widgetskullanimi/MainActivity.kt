a5b59b62cf70d8c825afa52f7566fba86e55231b
Tahsin Taydaş <56533800+taydastahsin@users.noreply.github.com>
2023-12-20 06:15:37 +0300

    20.12.2023

--- WidgetsKullanimi/app/src/main/java/taydastahsin/com/widgetskullanimi/MainActivity.kt
+++ WidgetsKullanimi/app/src/main/java/taydastahsin/com/widgetskullanimi/MainActivity.kt
@@ -1,13 +1,18 @@
 package taydastahsin.com.widgetskullanimi
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import taydastahsin.com.widgetskullanimi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var kontrol = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,21 +47,46 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.toggButton.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            kontrol =isChecked
+            if (kontrol) {
 
-            val secilenButton = findViewById<Button>(binding.toggButton.checkedButtonId)
-            val secilenYazi = secilenButton.text.toString()
-            Log.e("Sonuc","Buton :  $secilenYazi")
+                val secilenButton = findViewById<Button>(binding.toggButton.checkedButtonId)
+                val secilenYazi = secilenButton.text.toString()
+                Log.e("Sonuc", "Buton :  $secilenYazi")
+            }
 
         }
+
+        binding.buttonBaslat.setOnClickListener {
+            binding.progressBar.visibility = View.VISIBLE
+        }
+        binding.buttonDur.setOnClickListener {
+            binding.progressBar.visibility =View.INVISIBLE
+        }
+
+        binding.textViewProgresShow.text= binding.seekBar.progress.toString()
+
+        binding.seekBar.setOnSeekBarChangeListener(object  :OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+               binding.textViewProgresShow.text =progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
 
         binding.buttonGoster.setOnClickListener {
 
             val swichBilgi =binding.switch5.isChecked
             Log.e("Sonuc","Swich Durumu : ${swichBilgi.toString()}")
+            if (kontrol) {
 
-            val secilenButton = findViewById<Button>(binding.toggButton.checkedButtonId)
-            val secilenYazi = secilenButton.text.toString()
-            Log.e("Sonuc","Seçilen Buton :  $secilenYazi")
+                val secilenButton = findViewById<Button>(binding.toggButton.checkedButtonId)
+                val secilenYazi = secilenButton.text.toString()
+                Log.e("Sonuc", "Seçilen Buton :  $secilenYazi")
+            }
+            Log.e("Sonuc","Progres Durumu : ${binding.seekBar.progress}")
 
         }
 
