eda9c95b458c0ae546318c869c407318a2e7665f
Tahsin Taydaş <56533800+taydastahsin@users.noreply.github.com>
2023-12-27 18:25:20 +0300

    27.12.2023

--- KisilerUygulamasi/app/build.gradle.kts
+++ KisilerUygulamasi/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "taydastahsin.com.kisileruygulamasi"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("com.android.car.ui:car-ui-lib:2.5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- KisilerUygulamasi/app/src/main/java/taydastahsin/com/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
+++ KisilerUygulamasi/app/src/main/java/taydastahsin/com/kisileruygulamasi/ui/fragment/AnasayfaFragment.kt
@@ -1,11 +1,15 @@
 package taydastahsin.com.kisileruygulamasi.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView.OnQueryTextListener
+import android.widget.Toast
 import androidx.navigation.Navigation
+import com.google.android.material.snackbar.Snackbar
 import taydastahsin.com.kisileruygulamasi.R
 import taydastahsin.com.kisileruygulamasi.data.entity.Kisiler
 import taydastahsin.com.kisileruygulamasi.databinding.FragmentAnasayfaBinding
@@ -22,17 +26,27 @@ class AnasayfaFragment : Fragment() {
             Navigation.findNavController(it).navigate(R.id.anasayfa_kayit_gecis)
         }
 
-        binding.buttonDetay.setOnClickListener {
-            var kisi = Kisiler(1,"Tahsin","05392546511")
-            var gecis =AnasayfaFragmentDirections.anasayfaDetayGecis(kisi=kisi)
 
-            Navigation.findNavController(it).navigate(gecis)
 
-        }
+        binding.searchView.setOnSearchClickListener { object :OnQueryTextListener{
+            override fun onQueryTextChange(newText: String): Boolean {
+                ara(newText)
+                return  true
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                ara(query)
+               return  true
+            }
+        } }
 
 
         return binding.root
 
+    }
+
+    fun ara(girilenbilgi : String){
+        Log.e("Kişi arama",girilenbilgi)
     }
 
 
