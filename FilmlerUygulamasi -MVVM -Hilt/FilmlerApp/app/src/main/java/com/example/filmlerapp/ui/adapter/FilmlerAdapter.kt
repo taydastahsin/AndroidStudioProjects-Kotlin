fb90f8f8cbd52aca5b62988a48485f9b45759197
Tahsin Taydaş <56533800+taydastahsin@users.noreply.github.com>
2024-01-12 13:09:45 +0300

    12.01.2024

---
+++ FilmlerUygulamasi -MVVM -Hilt/FilmlerApp/app/src/main/java/com/example/filmlerapp/ui/adapter/FilmlerAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.filmlerapp.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.filmlerapp.R
+import com.example.filmlerapp.data.entity.Filmler
+import com.example.filmlerapp.databinding.CardTasarimBinding
+import com.example.filmlerapp.databinding.FragmentAnasayfaBinding
+import com.example.filmlerapp.ui.fragment.AnasayfaFragmentDirections
+import com.google.android.material.snackbar.Snackbar
+
+class FilmlerAdapter(var mContext:Context,var filmlerListesi:List<Filmler>)
+    : RecyclerView.Adapter<FilmlerAdapter.CardTasarimTutucu>() {
+
+    inner class CardTasarimTutucu(var tasarim:CardTasarimBinding) : RecyclerView.ViewHolder(tasarim.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardTasarimTutucu {
+        val binding:CardTasarimBinding = DataBindingUtil.inflate(LayoutInflater.from(mContext),R.layout.card_tasarim, parent, false)
+        return CardTasarimTutucu(binding)
+    }
+
+    override fun onBindViewHolder(holder: CardTasarimTutucu, position: Int) {
+        val film = filmlerListesi.get(position)
+        val t = holder.tasarim
+
+        t.imageViewFilm.setImageResource(
+            mContext.resources.getIdentifier(film.resim,"drawable",mContext.packageName))
+
+        t.filmNesnesi = film
+
+        t.cardViewFilm.setOnClickListener {
+            val gecis = AnasayfaFragmentDirections.detayGecis(film = film)
+            Navigation.findNavController(it).navigate(gecis)
+        }
+
+        t.buttonSepet.setOnClickListener {
+            Snackbar.make(it,"${film.ad} sepete eklendi",Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return filmlerListesi.size
+    }
+}
