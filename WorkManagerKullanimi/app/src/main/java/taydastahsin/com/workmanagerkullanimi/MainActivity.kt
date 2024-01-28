2638132804c84620809dd6e477a4c683a4740006
Tahsin Taydaş <56533800+taydastahsin@users.noreply.github.com>
2024-01-28 21:19:49 +0300

    28.01.2024

--- WorkManagerKullanimi/app/src/main/java/taydastahsin/com/workmanagerkullanimi/MainActivity.kt
+++ WorkManagerKullanimi/app/src/main/java/taydastahsin/com/workmanagerkullanimi/MainActivity.kt
@@ -2,9 +2,13 @@ package taydastahsin.com.workmanagerkullanimi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import taydastahsin.com.workmanagerkullanimi.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,9 +18,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonYap.setOnClickListener {
-            val istek= OneTimeWorkRequestBuilder<MyWorker>().build()
+            val calismaKosulu =Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+
+            val istek= OneTimeWorkRequestBuilder<MyWorker>()
+                .setInitialDelay(5,TimeUnit.SECONDS)
+                .setConstraints(calismaKosulu)
+                .build()
 
             WorkManager.getInstance(this).enqueue(istek)
+
+            WorkManager.getInstance(this).getWorkInfoByIdLiveData(istek.id).observe(this){
+                val durum = it.state.name
+                Log.e("Arka Plan Durum : ",durum)
+            }
         }
     }
 }
